2ec331b79d906c59b19a607022d4f1a032c0b60f
Arsam Sarabi <arsamsarabi@me.com>
2022-10-25 09:25:24 +0100

    fix(main): corrected description confirm key
    the shortcut key that confirmed the description was incorrectly displayed as CMD+D when it should have been control+D

--- got.sh
+++ got.sh
@@ -6,6 +6,6 @@ SCOPE=$(gum input --placeholder "scope")
 test -n "$SCOPE" && SCOPE="($SCOPE)"
 
 SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
-DESCRIPTION=$(gum write --placeholder "Details of this change (CMD+D to finish)")
+DESCRIPTION=$(gum write --placeholder "Details of this change (control+D to finish)")
 
 gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
